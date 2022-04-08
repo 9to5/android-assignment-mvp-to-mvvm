e621feeafe69a11fcc07e2c25b18915747734750
Jov Mit <mitrejcevski@gmail.com>
2022-04-08 23:41:48 +0200

    Leverage the shared test-double in the UI tests

--- app/src/androidTest/java/software/ninetofive/assignment_tests/main/MainScreenTest.kt
+++ app/src/androidTest/java/software/ninetofive/assignment_tests/main/MainScreenTest.kt
@@ -1,17 +1,30 @@
 package software.ninetofive.assignment_tests.main
 
+import android.content.SharedPreferences
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import dagger.hilt.android.testing.*
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import software.ninetofive.assignment_tests.R
+import software.ninetofive.assignment_tests.utils.InMemorySharedPreferences
+import software.ninetofive.assignment_tests.utils.SharedPreferencesModule
 
+@HiltAndroidTest
+@UninstallModules(SharedPreferencesModule::class)
 @RunWith(AndroidJUnit4::class)
 class MainScreenTest {
+
+    @get:Rule
+    val hiltRule = HiltAndroidRule(this)
+
+    @BindValue
+    val sharedPreferences: SharedPreferences = InMemorySharedPreferences()
 
     @Test
     fun defaultScreen() {
