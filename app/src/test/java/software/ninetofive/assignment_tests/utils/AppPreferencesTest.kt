5b53aa34d1d069968d058f891f81a535af3d648f
Jov Mit <mitrejcevski@gmail.com>
2022-04-08 18:06:24 +0200

    Leverage the seam to allow test-double substitution

--- app/src/test/java/software/ninetofive/assignment_tests/utils/AppPreferencesTest.kt
+++ app/src/test/java/software/ninetofive/assignment_tests/utils/AppPreferencesTest.kt
@@ -1,5 +1,7 @@
 package software.ninetofive.assignment_tests.utils
 
+import android.content.Context
+import android.content.SharedPreferences
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import software.ninetofive.assignment_tests.main.SelectedScreen
@@ -8,8 +10,15 @@ class AppPreferencesTest {
 
     @Test
     fun instantiate() {
-        val appPreferences = AppPreferences(null)
+        val appPreferences = TestableAppPreferences()
 
         assertEquals(SelectedScreen.SCREEN_C, appPreferences.getStartScreen())
+    }
+
+    class TestableAppPreferences : AppPreferences(null) {
+
+        override fun getPreferences(context: Context?): SharedPreferences? {
+            return super.getPreferences(context)
+        }
     }
 }
