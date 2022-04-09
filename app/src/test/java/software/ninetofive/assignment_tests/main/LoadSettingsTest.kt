d34fb3f02c30f9226d190e36d34f21cb9306ccde
Jov Mit <mitrejcevski@gmail.com>
2022-04-09 21:45:23 +0200

    Cover storing selected start screen value

--- app/src/test/java/software/ninetofive/assignment_tests/main/LoadSettingsTest.kt
+++ app/src/test/java/software/ninetofive/assignment_tests/main/LoadSettingsTest.kt
@@ -1,6 +1,9 @@
 package software.ninetofive.assignment_tests.main
 
+import io.reactivex.disposables.CompositeDisposable
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import software.ninetofive.assignment_tests.main.SelectedScreen.*
 import software.ninetofive.assignment_tests.utils.AppPreferences
@@ -10,6 +13,12 @@ class LoadSettingsTest {
 
     private val appPreferences = AppPreferences(InMemorySharedPreferences())
     private val presenter = MainPresenter(appPreferences)
+    private val disposable = CompositeDisposable()
+
+    @BeforeEach
+    fun setUp() {
+        disposable.add(presenter.dispose())
+    }
 
     @Test
     fun defaultSelectedScreen() {
@@ -26,5 +35,17 @@ class LoadSettingsTest {
         presenter.selectScreen(SCREEN_A)
 
         assertEquals(listOf(SCREEN_C, SCREEN_B, SCREEN_A), subscriber.values())
+    }
+
+    @Test
+    fun storingScreenSelection() {
+        presenter.selectScreen(SCREEN_B)
+
+        assertEquals(SCREEN_B, appPreferences.getStartScreen())
+    }
+
+    @AfterEach
+    fun tearDown() {
+        disposable.dispose()
     }
 }
