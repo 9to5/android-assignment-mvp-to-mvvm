717fe1c410ee2a42c9ac4c43b25db0b2246677f0
Jov Mit <mitrejcevski@gmail.com>
2022-04-09 21:30:50 +0200

    Improve formatting

--- app/src/test/java/software/ninetofive/assignment_tests/main/LoadSettingsTest.kt
+++ app/src/test/java/software/ninetofive/assignment_tests/main/LoadSettingsTest.kt
@@ -2,6 +2,7 @@ package software.ninetofive.assignment_tests.main
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import software.ninetofive.assignment_tests.main.SelectedScreen.*
 import software.ninetofive.assignment_tests.utils.AppPreferences
 import software.ninetofive.assignment_tests.utils.InMemorySharedPreferences
 
@@ -14,22 +15,16 @@ class LoadSettingsTest {
     fun defaultSelectedScreen() {
         val subscriber = presenter.onScreenSelectedFlowable.test()
 
-        assertEquals(SelectedScreen.SCREEN_C, subscriber.values().last())
+        assertEquals(SCREEN_C, subscriber.values().last())
     }
+
     @Test
     fun selectedScreenSelection() {
         val subscriber = presenter.onScreenSelectedFlowable.test()
 
-        presenter.selectScreen(SelectedScreen.SCREEN_B)
-        presenter.selectScreen(SelectedScreen.SCREEN_A)
+        presenter.selectScreen(SCREEN_B)
+        presenter.selectScreen(SCREEN_A)
 
-        assertEquals(
-            listOf(
-                SelectedScreen.SCREEN_C,
-                SelectedScreen.SCREEN_B,
-                SelectedScreen.SCREEN_A
-            ),
-            subscriber.values()
-        )
+        assertEquals(listOf(SCREEN_C, SCREEN_B, SCREEN_A), subscriber.values())
     }
 }
